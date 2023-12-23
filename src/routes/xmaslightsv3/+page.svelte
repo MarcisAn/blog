9f48a49567f8610cb2ef29a148e4590f98ef724f
Mārcis <60743431+MarcisAn@users.noreply.github.com>
2023-12-23 10:53:24 +0200

    add page

---
+++ src/routes/xmaslightsv3/+page.svelte
@@ -0,0 +1,39 @@
+<h1>3D eglītes lampiņas V3</h1>
+
+<h2>Ievads</h2>
+<p>Jau 3 gadus ziemassvētku tuvošanās man nozīmē lielu ņemšanos ar eglītes lampiņām. Ideju nospēru no angļu matemātikas
+jūtūbera "Stand up maths" un, gribētos teikt, ka uzlaboju.</p>
+<p>Visa būtība slēpjās principā, ka katrai lampiņai uz eglītes tiek noteiktas koordinātas 3D telpā. Tas paver iespēju uz lampiņām
+ atskaņot 3D animācijas.
+ Iedomājamies kubu - ir zināmas kuba virsotņu koordinātas, kā arī katras lampiņas koordinātas,
+ tātad varam noteikt, ka lampiņas, kas atrodās iekšā iedomātajā kubā tiek iedegtas kādā krāsā.
+ Iedomātais kubs kustās augšā, lejā. Tieši pēc šādas loģikas ir veidota lielākā daļa animāciju.
+ Šogad arī pirmo reizi izveidoju sistēmu, kas garāmgājējiem ļauj interaktīvi mainīt lampiņu animācijas.</p>
+
+<h2>Koordinātu noteikšana</h2>
+<h3>2021 v1</h3>
+<p>Šis bija neliels eksperiments klases eglītē, kas koordinātu noteikšanas ziņā būtiski atšķirās no turpmākajiem
+variantiem tādā ziņā, ka koordinātas netika noteiktas, tika noteikta tikai lampiņu secība uz ass. Kārtošanas algoritms
+iededz 2 lampiņas atšķirīgās krāsās un tad cilvēks nosaka kura lampiņa ir tālāk uz ass. Neskatoties uz neatbilstību virsrakstam,
+šī metode strādāja diezgan labi, lai gan sarežģītām animācijām tas nestrādātu,
+jo eglīte ir daudz augstāka par tās platumu, bet nosakot tikai secību uz ass mēs to nezinam, un ja šīs pseido-koordinātas
+mēģinātu attēlot vizuāli tad iegūtu kubu, kura iekšienē ir vienmērīgi izmētātas koordinātas.</p>
+
+<h3>2021 v2</h3>
+<p>Šoreiz tika noteiktas īstas koordinātas, uzņemot fotogrāfijas, kurā katrā ir ieslēga viena lampiņa, un tad par lampiņu
+koordinātām pieņemt iedegtās lampiņas atrašanās vietu uz fotogrāfijā, pikseļos. Parādijās problēma, kas sekos vēl ilgi - lampiņas
+nevar redzēt, jo priekšā ir egles zari. Daudzām lampiņām tāpēc nevarēja koordinātas, bet pēc tam es tās iebīdīju vietā
+'uz aci', balstoties uz blakus esošajām koordinātām.</p>
+
+<h3>2022</h3>
+<p>Šoreiz pacēlām mērīšanu 'uz aci' jaunā līmenī. Es noliku datoru starp mani un eglīti. Uz ekrāna kustējās līnija,
+kuru kontrolēju ar potenciometru. Iedegās lampiņa, pabīdu līniju uz ekrāna tā, lai tā sakrīt ar lampiņu un piefiksējam
+koordinātu uz ekrāna. Lai gan pats koordinātu iegūšanas process no malas izskatījās amizanti, rezultāti bija ļoti pieņemami.
+Turklāt process bija samērā ātrs. Arī šoreiz, protams, dažas lampiņas nevarēja redzēt caur egles zariem, bet te palīdzēja
+draugi, kas turēja slotas kātu egles otrā pusē, kur būtu jābūt lampiņai, lai no priekšas varētu vadīties pēc tā.</p>
+
+<h3>2023</h3>
+<p>Visticamāk precīzākā koordinātu ieguves metode līdz šim. Iepriekšējie mērījumi, visi tika veikti no viena punkta.
+Tāpēc perspektīva var nobīdīt mērījumus.</p>
+
+<p>Tāpēc šogad kustināsim pašu kameru.</p>
